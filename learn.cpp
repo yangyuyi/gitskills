7a2e10348964883172dfccbdfb9197e3f9acafd0
yangyuyi <TaroYoung2000@gmail.com>
2019-08-22 11:36:55 +0800

    learn.cpp modified

---
+++ learn.cpp
@@ -0,0 +1,14 @@
+/*
+* @Date    : 2019-08-19 15:45:18
+* @Author  : yangyuyi (TaroYoung@gmail.com)
+*/
+
+#include <iostream>
+
+using namespace std;
+int main(){
+	int a;
+	cin>>a;
+	cout<<a;
+	return 0;
+}
